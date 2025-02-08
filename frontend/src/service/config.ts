c472bf328d15bdbde8636cbaa1c59bc93f144100
Jason <abdulmbm002@mymail.sim.edu.sg>
2025-02-08 11:37:17 +0800

    change to domain api url

--- frontend/src/service/config.ts
+++ frontend/src/service/config.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://192.168.1.5:8000';
+// export const BASE_URL = 'http://192.168.1.8:8000';
+// export const BASE_URL = 'http://146.190.194.18';
+export const BASE_URL = 'https://hollytourism.live';
